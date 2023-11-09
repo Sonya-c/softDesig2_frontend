d3ad745b0a1e7ff40eba54453155a65be178cc7e
Sonya Castro <68860233+Sonya-c@users.noreply.github.com>
2023-11-09 21:10:01 +0000

    feat: layout navbar

--- src/utils/Layout.js
+++ src/utils/Layout.js
@@ -1,14 +1,21 @@
-import { Outlet, Link } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return <>
-        <main>
+        <Navbar bg="light" data-bs-theme="light">
+            <Container>
+            <Navbar.Brand href="#home">Diseño de software</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/">Users</Nav.Link>
+                    <Nav.Link href="/logs">Logs</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+        <Container fluid className="mh-100">
             <Outlet />
-        </main>
-        <footer>
-            <Link to="/">Users</Link>
-            <Link to="/logs">Logs</Link>
-        </footer>
+        </Container>
+        
     </>
 }
 
