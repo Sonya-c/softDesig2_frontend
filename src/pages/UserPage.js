dfcb549ac30ba62ec8f2840ef092555a504be351
Sonya Castro <68860233+Sonya-c@users.noreply.github.com>
2023-11-09 21:31:28 +0000

    feat: login page (sample table + search fields)

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,8 +1,74 @@
+import { useEffect, useState } from "react";
+import { Container, Table, Form, Row, Col, InputGroup, Button } from "react-bootstrap";
+
 function UserPage() {
+    const [users, setUsers] = useState([]);
+    
+    useEffect(() => {
+      setUsers([
+        {id: 1, type: 'cedula', names: 'juan', lastnames: 'perez', email: '', phone: '123'},
+        {id: 2, type: 'cedula', names: 'pedro pablo', lastnames: 'ramirez', email: 'pr@gmail.com', phone: ''},
+        {id: 3, type: 'cedula', names: 'maria', lastnames: 'perez', email: '', phone: '123'},
+        {id: 4, type: 'cedula', names: 'jose', lastnames: 'ramirez', email: 'jr@mail.com', phone: ''}
+      ]);
+    }, []);
+    
     return (
-      <div data-testid="user-page">
-        User Page
-      </div>
+      <Container data-testid="user-page">
+        <h1>Usuarios</h1>
+        <section>
+          <h2>Buscar usuario</h2>
+          
+          <Form inline>
+            <Row>
+              <Col xs="auto">
+                <Form.Control
+                  type="text"
+                  placeholder="Indetificación"
+                  className=" mr-sm-2"
+                />
+              </Col>
+              <Col xs="auto">
+                <Form.Control
+                  type="text"
+                  placeholder="Tipo de identificación"
+                  className=" mr-sm-2"
+                />
+              </Col>
+              <Col xs="auto">
+                <Button type="submit">Submit</Button>
+              </Col>
+            </Row>
+          </Form>
+        </section>
+        <section>
+          <h2>Todos los usuarios</h2>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Identificación</th>
+                <th>Nombre</th>
+                <th>Nombre(s)</th>
+                <th>Apellido(s)</th>
+                <th>Correo electrónico</th>
+                <th>Telefono</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => {
+                return <tr>
+                  <td>{user.id}</td>
+                  <td>{user.type}</td>
+                  <td>{user.names}</td>
+                  <td>{user.lastnames}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone}</td>
+                </tr>
+              })}
+            </tbody>
+          </Table>
+        </section>
+      </Container>
     );
   }
   
