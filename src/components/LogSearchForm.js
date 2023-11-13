f0bbd865a13c8fa3d02c9647e40a8e892857e25a
Sonya Castro <sonya.castro.gomez@gmail.com>
2023-11-12 20:05:05 -0500

    feat: add LogSearchForm handle input/submit

--- src/components/LogSearchForm.js
+++ src/components/LogSearchForm.js
@@ -1,20 +1,39 @@
 import { Form, Row, Col, Button } from "react-bootstrap";
+import { handleChange } from "../utils/utils";
+import { useState } from "react";
 
 export default function LogSearchForm({ logs }) {
-  return <Form>
+  const [formData, setFormData] = useState({
+    id: "",
+    type: "",
+    date: "",
+  });
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  }
+
+  return <Form onSubmit={handleSubmit}>
     <Row>
       <Col sm={true} className="mt-2">
         <Form.Group>
           <Form.Label>Fecha</Form.Label>
-          <Form.Control type="date" />
+          <Form.Control
+            type="date" />
         </Form.Group>
       </Col>
       <Col sm={true} className="mt-2">
         <Form.Group>
           <Form.Label htmlFor="form-id">Identificación</Form.Label >
-          <Form.Control as="select" id="search-form-id">
-            <option></option>
+          <Form.Control
+            as="select"
+            id="search-form-id"
+            value={formData.id}
+            name="id"
+            onChange={(event) => handleChange(setFormData, event)}
+          >
+            <option value=""></option>
             {logs.map((log) => {
               return <option key={"option" + log.id} value={log.id}>{log.id}</option>
             })}
@@ -24,12 +43,18 @@ export default function LogSearchForm({ logs }) {
       <Col sm={true} className="mt-2">
         <Form.Group>
           <Form.Label htmlFor="form-id-type">Tipo</Form.Label >
-          <Form.Control as="select" id="search-form-id">
-            <option></option>
-            <option>Read</option>
-            <option>Create</option>
-            <option>Create</option>
-            <option>Update</option>
+          <Form.Control
+            as="select"
+            id="search-form-id"
+            value={formData.type}
+            name="type"
+            onChange={(event) => handleChange(setFormData, event)}
+          >
+            <option value=""></option>
+            <option value="c">Create</option>
+            <option value="r">Read</option>
+            <option value="u">Update</option>
+            <option value="d">Create</option>
           </Form.Control>
         </Form.Group>
       </Col>
